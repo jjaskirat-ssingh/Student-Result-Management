40b770b445990bbc6cdcaa3f35ceeca18a330059
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2022-05-14 11:29:46 +0530

    Added updation functions

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ menu = """Please select one of the following options:
 7) Search for a subject.
 8) Exit.
 9) Search student using roll number
+10) Update Score
+11) Update Status
+12) Update Student name using rollno
+13) Update Student batch using rollno 
+14) Filter students of a particular batch
+
 
 Your selection: """
 welcome = "Welcome to the STUDENT RESULT MANAGEMENT INTERFACE!"
@@ -27,6 +33,8 @@ def print_subjects_list(heading, subjects, flag):
             print(f"{subject[0]}: {subject[1]}")
         elif flag == 2:
             print(f"{subject[0]}: {subject[1]}: {subject[2]}: {subject[3]}: {subject[4]}")
+        elif flag == 3:
+            print(f"{subject[0]}: {subject[1]}: {subject[2]}")
         else:
             print(f"{subject[0]}: {subject[1]}: {subject[3]}: {subject[4]}")
     print("---- \n")
@@ -36,8 +44,8 @@ def prompt_add_score_student():
     student_rollno = int(input("Roll Number: "))
     subject_id = input("Subject ID: ")
     marks = int(input("Marks: "))
-    status = int(input("Enter 1 for PASS and 0 for FAIL: "))
-    database.add_score_subject(student_rollno, subject_id, marks, status)
+    # status = int(input("Enter 1 for PASS and 0 for FAIL: "))
+    database.add_score_subject(student_rollno, subject_id, marks, 0)#, status)
 
 
 def prompt_get_score_all_subjects():
@@ -60,6 +68,31 @@ def prompt_student_rollno():
     search_term = input("Enter student's roll number: ")
     return database.search_student_rollno(search_term)
 
+def prompt_update_score():
+    rollno = int(input("Enter student's roll number: "))
+    subject_id = input("Enter Subject ID: ")
+    new_marks = int(input("Enter new marks of student: "))
+    return database.update_score(new_marks, rollno, subject_id)
+
+def prompt_update_status():
+    rollno = int(input("Enter student's roll number: "))
+    subject_id = input("Enter Subject ID: ")
+    new_status = int(input("Enter 1 for PASS and 0 for FAIL: "))
+    return database.update_status(new_status, rollno, subject_id)
+
+def prompt_update_student_name():
+    rollno = int(input("Enter student's roll number: "))
+    name = input("Enter correct name: ")
+    return database.update_student_name(name, rollno)
+
+def prompt_update_student_batch():
+    rollno = int(input("Enter student's roll number: "))
+    batch = input("Enter correct batch: ")
+    return database.update_student_batch(batch, rollno)
+
+def prompt_search_student_batch():
+    batch = input("Enter batch: ")
+    return database.search_student_batch(batch)
 
 print(welcome)
 database.create_tables()
@@ -92,5 +125,35 @@ while (user_input := int(input(menu))) != 8:
             print_subjects_list("Student found", students, 2)
         else:
             print("Sorry! No matching results found!")
+    elif user_input == 10:
+        updated_record = prompt_update_score()
+        if updated_record:
+            print_subjects_list("Student found", updated_record, 2)
+        else:
+            print("Sorry! Couldn't complete request. Kindly recheck rollno and Subject ID.")
+    elif user_input == 11:
+        updated_record = prompt_update_status()
+        if updated_record:
+            print_subjects_list("Student found", updated_record, 2)
+        else:
+            print("Sorry! Couldn't complete request. Kindly recheck rollno and Subject ID.")
+    elif user_input == 12:
+        updated_record = prompt_update_student_name()
+        if updated_record:
+            print_subjects_list("Scores in", updated_record, 3)
+        else:
+            print("No record exists for that rollno!")
+    elif user_input == 13:
+        updated_record = prompt_update_student_batch()
+        if updated_record:
+            print_subjects_list("Scores in", updated_record, 3)
+        else:
+            print("No record exists for that rollno!")
+    elif user_input == 14:
+        records = prompt_search_student_batch()
+        if records:
+            print_subjects_list("Scores in", records, 3)
+        else:
+            print("Nothing found!")
     else:
         print("Invalid input, please try again!")
--- database.py
+++ database.py
@@ -68,8 +68,184 @@ $$
 LANGUAGE plpgsql;
 """
 
+############################################################################
+
 # SEARCH_STUDENT_ROLLNO_CALL = """
 #     call SEARCH_STUDENT_ROLLNO(rollno);
+# """
+
+# STATUS_UPDATE_TRIGGER_FUNCTION = """
+# create or replace function update_status_func()
+# 	returns trigger
+# 	language plpgsql
+# 	as 
+# $$
+# begin
+# 	if NEW.marks > 33 then
+# 		update score set status = 1;
+# 	end if;
+# 	return new
+# end
+# $$
+# """
+
+# # create trigger status_update
+# # 	after update
+# # 	on scores
+# # 	for each row
+# # 	execute procedure update_status_func();
+# # $$
+# # """
+# # CREATE FUNCTION getSomeData()
+# # RETURNS trigger
+# # AS $$
+# # begin
+# # import subprocess
+# # subprocess.call(['/path/to/your/virtual/environment/bin/python3', '/some_folder/some_sub_folder/get_data.py'])
+# # end;
+# # $$
+# # LANGUAGE plpythonu;
+
+# STATUS_UPDATE_TRIGGER = """
+# CREATE FUNCTION STATUS_UPDATE_TRIGGER
+# RETURNS TRIGGER 
+# AS $$
+# BEGIN
+#   after update
+#   ON scores
+#   for each row
+#   execute function update_status_func();
+# END;
+# $$
+# LANGUAGE plpythonu;
+# """
+
+############################################################################
+
+UPDATE_SCORE = """
+--To update score
+
+CREATE OR REPLACE function UPDATE_SCORE(new_mark INTEGER, roll INTEGER, sub INTEGER) 
+RETURNS void  
+AS 
+$$ 
+DECLARE 
+    --ret record;
+BEGIN
+    UPDATE scores 
+    SET marks=UPDATE_SCORE.new_mark 
+    WHERE student_rollno=UPDATE_SCORE.roll AND subject_id=UPDATE_SCORE.sub; 
+    --RETURNING *;
+
+    --SELECT s.name, s.batch, sub.name, sc.marks, sc.status
+    --INTO ret.name, ret.batch, ret.sub_name, ret.marks, ret.status
+    --INTO ret
+    --from (students s join scores sc on s.rollno = sc.student_rollno)
+    --    join subjects sub on sc.subject_id = sub.id
+    --    where s.rollno = roll and sub.subject_id=sub;
+    --return ret;
+END;
+$$ 
+language plpgsql;
+"""
+
+UPDATE_STATUS = """
+CREATE OR REPLACE FUNCTION UPDATE_STATUS(new_status integer, roll integer, sub integer)
+RETURNS VOID AS 
+$$
+BEGIN
+    UPDATE scores 
+    SET status = new_status 
+    WHERE student_rollno = roll and subject_id = sub;
+END;
+$$
+LANGUAGE PLPGSQL
+"""
+
+UPDATE_STUDENT_NAME = """
+CREATE OR REPLACE FUNCTION UPDATE_STUDENT_NAME(new_name TEXT, roll INTEGER)
+RETURNS TABLE(auxrollno INTEGER, auxname TEXT, auxbatch TEXT) AS
+$$
+DECLARE 
+    --ret RECORD;
+BEGIN
+    UPDATE students 
+    SET name = new_name 
+    WHERE rollno = roll;
+
+    RETURN QUERY
+    SELECT * FROM students 
+    --INTO ret
+    WHERE rollno = roll;
+    --return ret;
+END;
+$$
+LANGUAGE PLPGSQL
+"""
+
+UPDATE_STUDENT_BATCH = """
+CREATE OR REPLACE FUNCTION UPDATE_STUDENT_BATCH(new_batch TEXT, roll INTEGER)
+RETURNS TABLE(auxrollno INTEGER, auxname TEXT, auxbatch TEXT) AS
+$$
+DECLARE 
+    --ret RECORD;
+BEGIN
+    UPDATE students 
+    SET batch = new_batch 
+    WHERE rollno = roll;
+
+    RETURN QUERY
+    SELECT * FROM students 
+    --INTO ret
+    WHERE rollno = roll;
+    --return ret;
+END;
+$$
+LANGUAGE PLPGSQL
+"""
+
+SEARCH_STUDENT_BATCH = """
+CREATE OR REPLACE FUNCTION SEARCH_STUDENT_BATCH(batch_no varchar)
+returns setof students
+AS 
+$$
+DECLARE ret record;
+    -- Cursor
+    entries CURSOR FOR 
+        SELECT * from students where batch=batch_no;
+BEGIN
+    OPEN entries;
+
+    LOOP
+    fetch entries into ret;
+        exit when ret = null;
+        return next ret;
+    END LOOP;
+    close entries;
+END;
+$$ LANGUAGE plpgsql
+"""
+
+# SEARCH_STUDENT_BATCH = """
+# CREATE OR REPLACE FUNCTION SEARCH_STUDENT_BATCH(batchno TEXT)
+# RETURNS refcursor AS '
+# DECLARE 
+#     ref refcursor;
+#     --entries CURSOR FOR
+#     --   SELECT * FROM students WHERE batch = batchno;
+# BEGIN 
+#     OPEN ref for SELECT * FROM students WHERE batch = batchno;
+ 
+#     LOOP
+#     FETCH entries into ref;
+#         EXIT WHEN ref = NULL;
+#         return NEXT ref;
+#     END LOOP;
+#     --CLOSE entries;
+#     return ref;
+# END;
+# '
+# LANGUAGE plpgsql
 # """
 
 dbname = os.environ.get("DATABASE_NAME")
@@ -105,6 +281,8 @@ def add_score_subject(student_rollno, subject_id, marks, status):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(INSERT_MARKS_SUBJECT, (student_rollno, subject_id, marks, status))
+            # cursor.execute(STATUS_UPDATE_TRIGGER_FUNCTION)
+            # cursor.execute(STATUS_UPDATE_TRIGGER)
 
 def get_scores_student(rollno):
     with connection:
@@ -123,4 +301,43 @@ def search_student_rollno(rollno):
         with connection.cursor() as cursor:
             cursor.execute(SEARCH_STUDENT_ROLLNO, (rollno,))
             cursor.execute("SELECT * FROM SEARCH_STUDENT_ROLLNO_( %s); ", (rollno,))
+            return cursor.fetchall()
+
+def update_score(new_marks, rollno, subject):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_SCORE, (new_marks, rollno, subject))
+            cursor.execute("SELECT FROM UPDATE_SCORE(%s, %s, %s); ", (new_marks, rollno, subject))
+            cursor.execute(SEARCH_STUDENT_ROLLNO, (rollno,))
+            cursor.execute("SELECT * FROM SEARCH_STUDENT_ROLLNO_( %s); ", (rollno,))
+            return cursor.fetchall()
+
+def update_status(new_status, rollno, subject):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_STATUS, (new_status, rollno, subject))
+            cursor.execute("SELECT FROM UPDATE_STATUS(%s, %s, %s); ", (new_status, rollno, subject))
+            cursor.execute(SEARCH_STUDENT_ROLLNO, (rollno,))
+            cursor.execute("SELECT * FROM SEARCH_STUDENT_ROLLNO_( %s); ", (rollno,))
+            return cursor.fetchall()
+
+def update_student_name(name, rollno):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_STUDENT_NAME, (name, rollno))
+            cursor.execute("SELECT * FROM UPDATE_STUDENT_NAME(%s, %s)", (name, rollno))
+            return cursor.fetchall()
+
+def update_student_batch(batch, rollno):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_STUDENT_BATCH, (batch, rollno))
+            cursor.execute("SELECT * FROM UPDATE_STUDENT_BATCH(%s, %s)", (batch, rollno))
+            return cursor.fetchall()
+
+def search_student_batch(batch):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SEARCH_STUDENT_BATCH, (batch,))
+            cursor.execute("SELECT * FROM SEARCH_STUDENT_BATCH(%s); ", (batch,))
             return cursor.fetchall()
